c92f3e64be37ca0afbd92b39b338ed23c2bba590
Jarod Towner <towja@icloud.com>
2016-09-17 18:58:53 +0930

    Checks if player is colliding with the bar, if it is, and the player is holding e, the bar will slowly increment.

---
+++ projectTorch/Assets/Scripts/jarodScripts/elevatorProgressBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class elevatorProgressBar : MonoBehaviour {
+
+    public string interactKey;
+    public float waitTime;
+    public Image image;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    
+    //
+    void OnTriggerStay(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            if (Input.GetKey(interactKey))
+            {
+                image.fillAmount += 1.0f / waitTime * Time.deltaTime;
+            }
+        }
+    }
+}
