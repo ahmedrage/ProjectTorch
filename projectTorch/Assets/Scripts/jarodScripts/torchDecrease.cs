2f403b163a70b6631c084c82a21738cc0b8e639a
Jarod Towner <towja@icloud.com>
2016-09-17 23:18:07 +0930

    Added some functionality to the main menu and wrote a script for torch life decreasing.

---
+++ projectTorch/Assets/Scripts/jarodScripts/torchDecrease.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class torchDecrease : MonoBehaviour {
+
+    public float lifeTime;
+    public Light lamp;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lamp.intensity -= 1.0f / lifeTime * Time.deltaTime;
+    }
+}
