450dc865cd71be31a5fd4d6675245bf23d3c3e1f
Jarod Towner <towja@icloud.com>
2016-09-17 23:53:26 +0930

    Added lantern flickering and decreasing. Deleted some unnecessary files.

---
+++ projectTorch/Assets/Scripts/jarodScripts/torch/torchFlicker.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class torchFlicker : MonoBehaviour {
+
+    public Light lantern;
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lantern.intensity -= 0.5f / 1.0f * Time.deltaTime;
+        lantern.intensity += 0.5f / 1.0f * Time.deltaTime;
+    }
+}
