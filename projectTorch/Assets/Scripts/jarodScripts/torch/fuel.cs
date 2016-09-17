2b58295e602d13ac5bf09f6cfdd06674559beb15
Jarod Towner <towja@icloud.com>
2016-09-18 02:19:21 +0930

    Added the ability to refuel the lantern.

---
+++ projectTorch/Assets/Scripts/jarodScripts/torch/fuel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class batteries : MonoBehaviour {
+
+    public float fuelValue;
+    public Light lantern;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay(Collider coll) {
+        if (coll.gameObject.tag == "Player")
+        {
+            //Run code to pick up battery here
+            lantern.intensity += fuelValue;
+        }
+    }
+}
